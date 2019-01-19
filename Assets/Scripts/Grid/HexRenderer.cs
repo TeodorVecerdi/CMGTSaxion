f547124342bd92ba4541d9669702a8fc70ab27e1
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2019-01-19 22:59:08 +0200

    Add maze reveal and UV mapping on cells

--- Assets/Scripts/Grid/HexRenderer.cs
+++ Assets/Scripts/Grid/HexRenderer.cs
@@ -6,10 +6,14 @@ public class HexRenderer : MonoBehaviour {
     private Mesh mesh;
     private List<Vector3> vertices;
     private List<int> triangles;
+    public List<Vector2> uvs;
+
+    private Vector2 UV_TEXTURE_SIZE = new Vector2(1f / 3f, 1f / 1f);
 
     private void Awake() {
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "Cell Mesh";
+        uvs = new List<Vector2>();
         vertices = new List<Vector3>();
         triangles = new List<int>();
     }
@@ -18,6 +22,7 @@ public class HexRenderer : MonoBehaviour {
         mesh.Clear();
         vertices.Clear();
         triangles.Clear();
+        uvs.Clear();
         for (int x = 0; x < grid.Width; x++) {
             for (int y = 0; y < grid.Height; y++) {
                 if(grid[x, y] != null)
@@ -26,13 +31,16 @@ public class HexRenderer : MonoBehaviour {
         }
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
         mesh.RecalculateNormals();
     }
 
     private void AddHex(HexCell hexCell) {
+        Vector2 UVCenter = UV_TEXTURE_SIZE*0.5f;
         Vector3 center = new Vector3(hexCell.WorldX + hexCell.Radius * HexUtils.INNER_CONSTANT, hexCell.WorldY + hexCell.Radius, 0f);
         for (int i = 0; i < 6; i++) {
             AddTriangle(center, center + HexUtils.Vertices[i % 6] * hexCell.Radius, center + HexUtils.Vertices[(i + 1) % 6] * hexCell.Radius);
+            AddUvs(i, UVCenter, HexCell.GetTexture(hexCell));
         }
     }
 
@@ -44,5 +52,16 @@ public class HexRenderer : MonoBehaviour {
         triangles.Add(index);
         triangles.Add(index + 1);
         triangles.Add(index + 2);
+    }
+
+    private void AddUvs(int index, Vector2 center, Vector2 texture) {
+        center += texture*UV_TEXTURE_SIZE;
+        uvs.Add(center);
+        uvs.Add(center + ToVector2(HexUtils.Vertices[index % 6]) * UV_TEXTURE_SIZE / 2f);
+        uvs.Add(center + ToVector2(HexUtils.Vertices[(index+1) % 6]) * UV_TEXTURE_SIZE / 2f);
+    }
+
+    private Vector2 ToVector2(Vector3 v) {
+        return new Vector2(v.x, v.y);
     }
 }
--- Assets/Scripts/Misc/PlayerController.cs
+++ Assets/Scripts/Misc/PlayerController.cs
@@ -1,36 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using static Logger;
 using CellIndex = System.ValueTuple<int, int, int>;
 
 public class PlayerController : MonoBehaviour {
     public SpriteRenderer[] ArrowRenderers;
     public GridController GridController;
+    public TargetFollow CameraFollow;
 
     private readonly Vector3 CenterOffset = new Vector3(HexUtils.INNER_CONSTANT, 1f, 0);
+    private bool zoomedOut;
 
-    private int gridX = 0;
-    private int gridY = 0;
-    
+    private int gridX;
+    private int gridY;
+
     private void Start() {
         Vector3 arrayToWorld = HexUtils.ArrayToWorldCoordinates(0, 0, GridController.CellRadius);
         Vector3 spawnLocation = arrayToWorld + CenterOffset * GridController.CellRadius;
         transform.position = spawnLocation;
-        UpdateArrows();
+
+        List<CellIndex> neighbours = GridController.Grid.GetPassableNeighbours(gridX, gridY);
+        GridController.Grid[0, 0].Visible = true;
+        UpdateArrows(neighbours);
+        MakeNeighboursVisible(neighbours);
     }
 
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.E)) TryMove(0);
-        if (Input.GetKeyDown(KeyCode.D)) TryMove(1);
-        if (Input.GetKeyDown(KeyCode.C)) TryMove(2);
-        if (Input.GetKeyDown(KeyCode.Z)) TryMove(3);
-        if (Input.GetKeyDown(KeyCode.A)) TryMove(4);
-        if (Input.GetKeyDown(KeyCode.Q)) TryMove(5);
+        if (!zoomedOut) { //The player needs to be zoomed-in in order to be able to move
+            if (Input.GetKeyDown(KeyCode.E)) TryMove(0);
+            if (Input.GetKeyDown(KeyCode.D)) TryMove(1);
+            if (Input.GetKeyDown(KeyCode.C)) TryMove(2);
+            if (Input.GetKeyDown(KeyCode.Z)) TryMove(3);
+            if (Input.GetKeyDown(KeyCode.A)) TryMove(4);
+            if (Input.GetKeyDown(KeyCode.Q)) TryMove(5);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //Zoom out/in
+            zoomedOut = !zoomedOut;
+            //Show whole maze
+            if (zoomedOut) {
+                CameraFollow.target = null;
+                float orthoSize = GridController.CellRadius + GridController.CellRadius * (GridController.Height - 1) * 0.75f;
+                CameraFollow.gameObject.GetComponent<Camera>().orthographicSize = orthoSize;
+                CameraFollow.transform.position = new Vector3(GridController.Height * GridController.CellRadius * HexUtils.INNER_CONSTANT, orthoSize, -10f);
+            }
+            //Show player
+            else {
+                //reset camera size and target
+                CameraFollow.gameObject.GetComponent<Camera>().orthographicSize = GridController.CellRadius * 1.5f;
+                CameraFollow.transform.position = transform.position + Vector3.back * 10;
+                CameraFollow.target = transform;
+            }
+        }
     }
 
-    private void UpdateArrows() {
+    private void TryMove(int direction) {
         List<CellIndex> neighbours = GridController.Grid.GetPassableNeighbours(gridX, gridY);
+        foreach (var neighbour in neighbours) {
+            if (neighbour.Item3 == direction) {
+                Move(direction);
+                return;
+            }
+        }
+    }
+
+    private void Move(int direction) {
+        //Move the player to the new cell and update internal array position
+        transform.position += HexUtils.DistanceToNeighbours[direction] * GridController.CellRadius;
+        gridX += HexUtils.NeighbourDelta[gridY % 2][direction].Item1;
+        gridY += HexUtils.NeighbourDelta[gridY % 2][direction].Item2;
+
+        //Get new neighbours after move
+        List<CellIndex> neighbours = GridController.Grid.GetPassableNeighbours(gridX, gridY);
+
+        //Make accessible neighbours visible
+        MakeNeighboursVisible(neighbours);
+
+        //Update arrows to show the directions that the player can move in 
+        UpdateArrows(neighbours);
+
+        //Redraw meshes
+        GridController.UpdateMeshes();
+    }
+
+    private void UpdateArrows(List<CellIndex> neighbours = null) {
+        //Get neighbours if not received through parameter
+        if (neighbours == null)
+            neighbours = GridController.Grid.GetPassableNeighbours(gridX, gridY);
+
+        //Show only the arrows that point to cells accessible by the player.
         foreach (var arrowRenderer in ArrowRenderers) {
             arrowRenderer.gameObject.SetActive(false);
         }
@@ -39,16 +96,12 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    private void TryMove(int direction) {
-        List<CellIndex> neighbours = GridController.Grid.GetPassableNeighbours(gridX, gridY);
-        foreach (var neighbour in neighbours) {
-            if (neighbour.Item3 == direction) {
-                transform.position += HexUtils.DistanceToNeighbours[direction] * GridController.CellRadius;
-                gridX += HexUtils.NeighbourDelta[gridY % 2][direction].Item1;
-                gridY += HexUtils.NeighbourDelta[gridY % 2][direction].Item2;
-                UpdateArrows();
-                return;
-            }
-        }
+    private void MakeNeighboursVisible(List<CellIndex> neighbours = null) {
+        //Get neighbours if not received through parameter
+        if (neighbours == null)
+            neighbours = GridController.Grid.GetPassableNeighbours(gridX, gridY);
+
+        foreach (var neighbour in neighbours)
+            GridController.Grid.Cells[neighbour.Item1, neighbour.Item2].Visible = true;
     }
 }
